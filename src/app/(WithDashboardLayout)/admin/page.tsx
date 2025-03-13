b2f7da386c4e0bd86ee6aa6925d72095f3a2fb90
Md Sakib al hasan <mdsakibalhasanprogrammer1@gmail.com>
2025-03-13 15:38:28 +0600

    add Folder

---
+++ src/app/(WithDashboardLayout)/admin/page.tsx
@@ -0,0 +1,9 @@
+const Adminpage = () => {
+  return (
+    <div>
+      <p className="text-4xl">adminpage</p>
+    </div>
+  )
+}
+
+export default Adminpage
---
+++ src/app/(WithDashboardLayout)/user/page.tsx
@@ -0,0 +1,9 @@
+const Userpage = () => {
+  return (
+    <div>
+      <p className="text-4xl">Userpage</p>
+    </div>
+  )
+}
+
+export default Userpage
